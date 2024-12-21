2b6c745567c12824a6b1dbd8792ebf4735e3f612
Polly Bel <polialekseevna80@gmail.com>
2024-12-21 11:27:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,14 @@ from imagi.Image_Processor import ImageProcessor
 def main():
     
     image_path = input("Введите путь к изображению: ")
-    
     ih = ImageHandler(image_path)
-   
     ih.load_image()
-    # преобразуем изображение в формат JPG и сохраняем его в файл output.jpg
-    ih.jpg("file.jpg") 
-    # поворачиваем изображение на 45 градусов
-    ih.rotate_image(45)
-    # создаем объект класса ImageProcessor, передав ему изображение из ImageHandler для дальнейшей обработки
+    ih.jpg("file.jpg")  # преобразуем изображение в формат jpg
+    ih.rotate_image(45)# поворачиваем изображение на 45 градусов
     ip = ImageProcessor(ih.get_image())
-    # применяем фильтр повышения резкости к изображению
-    ip.sharpen_filter()
-    # добавляем рамку шириной 15 пикселей вокруг изображения
-    ip.add_border(15)
-    # сохранение обработанного изображения
-    ip.save_image("change_file.jpg")
+    ip.sharpen_filter()# применяем фильтр повышения резкости к изображению
+    ip.add_border(15)   # добавляем рамку шириной 15 пикселей вокруг изображения
+    ip.save_image("change_file.jpg") # сохранение обработанного изображения
 
-if __name__ == "__main__":
+if __name__ == "__main__": ip.save_image("change_file.jpg")
     main() 
